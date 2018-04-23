6ab4a4022bcbf9030a8bf09df74741dcd8d3a655
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to customize a Choropleth colorizer's range distribution
+
+
+<p>This example demonstrates how to customize a Choropleth colorizer's range distribution.</p>
+<p>It is possible using one of the following range distributions.<br />- <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapLinearRangeDistributiontopic">Linear</a>.<br />- <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapLogarithmicRangeDistributiontopic">Logarithmic</a>.<br />- <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapExponentialRangeDistributiontopic">Exponential</a>.<br />To use one of them assign a required distribution to the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapChoroplethColorizer_RangeDistributiontopic">ChoroplethColorizer.RangeDistribution</a> property. Note that the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapChoroplethColorizer_ApproximateColorstopic">ChoroplethColorizer.ApproximateColors</a> property should be set to <strong>true</strong>.</p>
+
+<br/>
+
+
---
+++ VB/RangeDistributions/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Windows
+Imports DevExpress.Xpf.Map
+
+Namespace RangeDistributions
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub factor_EditValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.EditValueChangedEventArgs)
+            Dim distribution = TryCast(colorizer.RangeDistribution, EquationRangeDistribution)
+            If distribution Is Nothing Then
+                Return
+            End If
+
+            distribution.Factor = CDbl(seFactor.Value)
+        End Sub
+
+        Private Sub rangeDistribution_SelectedIndexChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            seFactor.IsEnabled = If(rangeDistribution.SelectedItem.Equals(linear), False, True)
+
+            Dim distribution = TryCast(colorizer.RangeDistribution, EquationRangeDistribution)
+            If distribution Is Nothing Then
+                Return
+            End If
+
+            seFactor.Value = CDec(distribution.Factor)
+        End Sub
+    End Class
+End Namespace
