7c63c0975d2598eff63d5527bce1c45663528951
DevExpressExampleBot <fakeEmail>
2022-12-18 17:42:07 +0400

    Source auto update [skip ci]

--- VB/RangeDistributions/MainWindow.xaml.vb
+++ VB/RangeDistributions/MainWindow.xaml.vb
@@ -1,35 +1,29 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.Map
 
 Namespace RangeDistributions
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub factor_EditValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.EditValueChangedEventArgs)
-			Dim distribution = TryCast(colorizer.RangeDistribution, EquationRangeDistribution)
-			If distribution Is Nothing Then
-				Return
-			End If
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			distribution.Factor = CDbl(seFactor.Value)
-		End Sub
+        Private Sub factor_EditValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.EditValueChangedEventArgs)
+            Dim distribution = TryCast(Me.colorizer.RangeDistribution, EquationRangeDistribution)
+            If distribution Is Nothing Then Return
+            distribution.Factor = CDbl(Me.seFactor.Value)
+        End Sub
 
-		Private Sub rangeDistribution_SelectedIndexChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			seFactor.IsEnabled = If(rangeDistribution.SelectedItem.Equals(linear), False, True)
-
-			Dim distribution = TryCast(colorizer.RangeDistribution, EquationRangeDistribution)
-			If distribution Is Nothing Then
-				Return
-			End If
-
-			seFactor.Value = CDec(distribution.Factor)
-		End Sub
-	End Class
+        Private Sub rangeDistribution_SelectedIndexChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.seFactor.IsEnabled = If(Me.rangeDistribution.SelectedItem.Equals(Me.linear), False, True)
+            Dim distribution = TryCast(Me.colorizer.RangeDistribution, EquationRangeDistribution)
+            If distribution Is Nothing Then Return
+            Me.seFactor.Value = CDec(distribution.Factor)
+        End Sub
+    End Class
 End Namespace
